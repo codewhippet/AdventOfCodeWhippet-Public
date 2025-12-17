05133a5ce1a7cba285d57e488e3b755d9a77f4a3
codewhippet <10889487+codewhippet@users.noreply.github.com>
2025-12-17 23:16:49 +0000

    Optimisation for 2025 Day 10. Finally running in under a second on the Pi Zero. 64-bit values are slow! Thankfully this puzzle only needs 32-bit accuracy so we can use those for a speed boost. Also fully codified the assumption that we have a fully non-zero diagonal after enough shuffling, which tidies the code a bit.

--- AoC2025/Puzzle10.cpp
+++ AoC2025/Puzzle10.cpp
@@ -15,8 +15,8 @@ namespace Puzzle10_2025_Types
 
 	struct Counters
 	{
-		vector<int16_t> TargetJolts;
-		vector<vector<int8_t>> Buttons;
+		vector<int32_t> TargetJolts;
+		vector<vector<int32_t>> Buttons;
 	};
 }
 
@@ -70,11 +70,11 @@ static vector<uint32_t> ParseMachineButtons(const string& line)
 	return buttons;
 }
 
-static vector<int16_t> ParseTargetJolts(const string& line)
+static vector<int32_t> ParseTargetJolts(const string& line)
 {
 	const regex joltFormat(R"(\d+)");
 
-	vector<int16_t> jolts;
+	vector<int32_t> jolts;
 	for (sregex_iterator it = sregex_iterator(line.begin() + line.find('{'), line.end(), joltFormat); it != sregex_iterator{}; ++it)
 	{
 		jolts.push_back((int16_t)stoi(it->str()));
@@ -91,9 +91,9 @@ static Machine ParseMachine(const string& line)
 	return m;
 }
 
-static vector<int8_t> ParseCounterButton(const string& match)
+static vector<int32_t> ParseCounterButton(const string& match)
 {
-	vector<int8_t> button;
+	vector<int32_t> button;
 	for (const char c : match)
 	{
 		if (isdigit(c))
@@ -104,11 +104,11 @@ static vector<int8_t> ParseCounterButton(const string& match)
 	return button;
 }
 
-static vector<vector<int8_t>> ParseCounterButtons(const string& line)
+static vector<vector<int32_t>> ParseCounterButtons(const string& line)
 {
 	const regex buttonFormat(R"(\([^\)]+\))");
 
-	vector<vector<int8_t>> buttons;
+	vector<vector<int32_t>> buttons;
 	for (sregex_iterator it = sregex_iterator(line.begin(), line.end(), buttonFormat); it != sregex_iterator{}; ++it)
 	{
 		buttons.push_back(ParseCounterButton(it->str()));
@@ -125,25 +125,25 @@ static Counters ParseCounters(const string& line)
 	return c;
 }
 
-static void Scale(vector<int64_t>* v, int64_t s)
+static void Scale(vector<int32_t>* v, int32_t s)
 {
-	ranges::for_each(*v, [s](int64_t& i) { i *= s; });
+	ranges::for_each(*v, [s](int32_t& i) { i *= s; });
 }
 
-static vector<vector<int64_t>> CountersToAugmentedMatrix(const Counters& c)
+static vector<vector<int32_t>> CountersToAugmentedMatrix(const Counters& c)
 {
-	vector<vector<int64_t>> m(c.TargetJolts.size(), vector<int64_t>(c.Buttons.size() + 1));
+	vector<vector<int32_t>> m(c.TargetJolts.size(), vector<int32_t>(c.Buttons.size() + 1));
 
 	// Each button is a column
-	for (int64_t column = 0; column < (int64_t)c.Buttons.size(); column++)
+	for (size_t column = 0; column < c.Buttons.size(); column++)
 	{
-		for (int8_t row : c.Buttons[column])
+		for (int32_t row : c.Buttons[column])
 		{
 			m[row][column] = 1;
 		}
 	}
 
-	for (int64_t i = 0; i < (int64_t)c.TargetJolts.size(); i++)
+	for (size_t i = 0; i < c.TargetJolts.size(); i++)
 	{
 		m[i].back() = c.TargetJolts[i];
 	}
@@ -151,30 +151,30 @@ static vector<vector<int64_t>> CountersToAugmentedMatrix(const Counters& c)
 	return m;
 }
 
-static vector<int64_t> CountersToConstraints(const Counters& c)
+static vector<int32_t> CountersToConstraints(const Counters& c)
 {
-	vector<int64_t> constraints(c.Buttons.size(), numeric_limits<int64_t>::max());
-	for (int64_t button = 0; button < (int64_t)c.Buttons.size(); button++)
+	vector<int32_t> constraints(c.Buttons.size(), numeric_limits<int32_t>::max());
+	for (size_t button = 0; button < c.Buttons.size(); button++)
 	{
-		for (int8_t counter : c.Buttons[button])
+		for (int32_t counter : c.Buttons[button])
 		{
-			constraints[button] = min<int64_t>(constraints[button], c.TargetJolts[counter]);
+			constraints[button] = min<int32_t>(constraints[button], c.TargetJolts[counter]);
 		}
 	}
 	return constraints;
 }
 
-static bool IsNonZeroRow(const vector<int64_t>& v)
+static bool IsNonZeroRow(const vector<int32_t>& v)
 {
-	return ranges::count(v, 0) != (int64_t)v.size();
+	return ranges::count(v, 0) != (int32_t)v.size();
 }
 
-static bool IsZeroRow(const vector<int64_t>& v)
+static bool IsZeroRow(const vector<int32_t>& v)
 {
-	return ranges::count(v, 0) == (int64_t)v.size();
+	return ranges::count(v, 0) == (int32_t)v.size();
 }
 
-static vector<int64_t> Reduce(vector<int64_t> rowToReduce, vector<int64_t> reducingRow, int64_t reducingColumn)
+static vector<int32_t> Reduce(vector<int32_t> rowToReduce, vector<int32_t> reducingRow, size_t reducingColumn)
 {
 	if (rowToReduce[reducingColumn] == 0)
 	{
@@ -182,18 +182,19 @@ static vector<int64_t> Reduce(vector<int64_t> rowToReduce, vector<int64_t> reduc
 		return rowToReduce;
 	}
 
+	// Make sure both rows have a positive leading value
 	assert(reducingRow[reducingColumn] > 0);
 	if (rowToReduce[reducingColumn] < 0)
 	{
 		Scale(&rowToReduce, -1);
 	}
 
-	int64_t scaleTo = lcm(rowToReduce[reducingColumn], reducingRow[reducingColumn]);
+	int32_t scaleTo = lcm(rowToReduce[reducingColumn], reducingRow[reducingColumn]);
 	Scale(&rowToReduce, scaleTo / rowToReduce[reducingColumn]);
 	Scale(&reducingRow, scaleTo / reducingRow[reducingColumn]);
 	assert(rowToReduce[reducingColumn] == reducingRow[reducingColumn]);
 
-	for (int i = 0; i < (int64_t)rowToReduce.size(); i++)
+	for (size_t i = 0; i < rowToReduce.size(); i++)
 	{
 		rowToReduce[i] -= reducingRow[i];
 	}
@@ -201,17 +202,20 @@ static vector<int64_t> Reduce(vector<int64_t> rowToReduce, vector<int64_t> reduc
 	return rowToReduce;
 }
 
-static void ReduceAndTrim(vector<vector<int64_t>>* pm)
+static bool ReduceAndTrim(vector<vector<int32_t>>* pm)
 {
-	vector<vector<int64_t>>& m = *pm;
-	int64_t diagonalEnd = min<int64_t>(m.size(), m.front().size() - 1);
-	for (int64_t diagonal = 0; diagonal < diagonalEnd; diagonal++)
+	vector<vector<int32_t>> m = *pm;
+
+	size_t diagonalEnd = min(m.size(), m.front().size() - 1);
+	for (size_t diagonal = 0; diagonal < diagonalEnd; diagonal++)
 	{
-		// Put empty rows at the bottom
-		partition(m.begin() + diagonal, m.end(), IsNonZeroRow);
+		// Put empty rows at the bottom; might cause us to stop reducing early
+		auto endOfNonZeroRows = partition(m.begin() + diagonal, m.end(), IsNonZeroRow);
+		if (diagonal >= (size_t)distance(m.begin(), endOfNonZeroRows))
+			break;
 
 		// Find a row with a non-zero element in the column
-		for (int64_t reducingRow = diagonal; reducingRow < (int64_t)m.size(); reducingRow++)
+		for (size_t reducingRow = diagonal; reducingRow < m.size(); reducingRow++)
 		{
 			if (m[reducingRow][diagonal] != 0)
 			{
@@ -220,12 +224,18 @@ static void ReduceAndTrim(vector<vector<int64_t>>* pm)
 			}
 		}
 
+		if (m[diagonal][diagonal] == 0)
+		{
+			// Could not find a non-zero pivot
+			return false;
+		}
+
 		if (m[diagonal][diagonal] < 0)
 		{
 			Scale(&m[diagonal], -1);
 		}
 
-		for (int64_t rowToReduce = diagonal + 1; rowToReduce < (int64_t)m.size(); rowToReduce++)
+		for (size_t rowToReduce = diagonal + 1; rowToReduce < m.size(); rowToReduce++)
 		{
 			m[rowToReduce] = Reduce(m[rowToReduce], m[diagonal], diagonal);
 		}
@@ -233,44 +243,33 @@ static void ReduceAndTrim(vector<vector<int64_t>>* pm)
 
 	// Get rid of the empty rows
 	m.erase(remove_if(m.begin(), m.end(), IsZeroRow), m.end());
+
+	*pm = m;
+	return true;
 }
 
-static void TrySolvedConstrained(const Counters& counters,
-	const vector<vector<int64_t>>& m,
-	int64_t rowToSolve,
-	int64_t nextUnknown,
-	const vector<int64_t>& constraints,
-	vector<int64_t>* alreadyAssigned,
-	int64_t *minimumPresses)
+static void TrySolvedConstrained(const vector<vector<int32_t>>& m,
+	size_t rowToSolve,
+	size_t nextUnknown,
+	const vector<int32_t>& constraints,
+	vector<int32_t>* alreadyAssigned,
+	int32_t *minimumPresses)
 {
-	vector<int64_t>& solution = *alreadyAssigned;
+	vector<int32_t>& solution = *alreadyAssigned;
 
-	if (nextUnknown == -1)
+	if ((nextUnknown + 1) == 0)
 	{
-		vector<int16_t> accumulatedJolts(counters.TargetJolts.size(), 0);
-		for (int64_t button = 0; button < (int64_t)counters.Buttons.size(); button++)
-		{
-			for (int8_t counter : counters.Buttons[button])
-			{
-				accumulatedJolts[counter] += (int16_t)solution[button];
-			}
-		}
-
-		if (accumulatedJolts == counters.TargetJolts)
-		{
-			*minimumPresses = min(*minimumPresses, ranges::fold_left(solution, 0, plus{}));
-		}
-
+		*minimumPresses = min(*minimumPresses, ranges::fold_left(solution, 0, plus{}));
 		return;
 	}
 
 	// If the matrix isn't big enough we're going to need to guess
 	if (nextUnknown > rowToSolve)
 	{
-		for (int64_t guess = 0; guess <= constraints[nextUnknown]; guess++)
+		for (int32_t guess = 0; guess <= constraints[nextUnknown]; guess++)
 		{
 			solution[nextUnknown] = guess;
-			TrySolvedConstrained(counters, m, rowToSolve, nextUnknown - 1, constraints, alreadyAssigned, minimumPresses);
+			TrySolvedConstrained(m, rowToSolve, nextUnknown - 1, constraints, alreadyAssigned, minimumPresses);
 		}
 		return;
 	}
@@ -279,18 +278,18 @@ static void TrySolvedConstrained(const Counters& counters,
 	if (m[rowToSolve][nextUnknown] == 0)
 	{
 		// We're not able to solve directly so we need to guess
-		for (int64_t guess = 0; guess <= constraints[nextUnknown]; guess++)
+		for (int32_t guess = 0; guess <= constraints[nextUnknown]; guess++)
 		{
 			solution[nextUnknown] = guess;
-			TrySolvedConstrained(counters, m, rowToSolve - 1, nextUnknown - 1, constraints, alreadyAssigned, minimumPresses);
+			TrySolvedConstrained(m, rowToSolve - 1, nextUnknown - 1, constraints, alreadyAssigned, minimumPresses);
 		}
 		return;
 	}
 
-	int64_t rowTargetSum = m[rowToSolve].back();
+	int32_t rowTargetSum = m[rowToSolve].back();
 
 	// Eliminate everything we already know about
-	for (int64_t known = nextUnknown + 1; known < (int64_t)solution.size(); known++)
+	for (size_t known = nextUnknown + 1; known < solution.size(); known++)
 	{
 		rowTargetSum -= m[rowToSolve][known] * solution[known];
 		//m[rowToSolve][known] = 0; // For debugging sanity
@@ -303,7 +302,7 @@ static void TrySolvedConstrained(const Counters& counters,
 		return;
 	}
 
-	int64_t tentativeSolution = rowTargetSum / m[rowToSolve][nextUnknown];
+	int32_t tentativeSolution = rowTargetSum / m[rowToSolve][nextUnknown];
 	if (tentativeSolution < 0)
 	{
 		// We're only looking for positive solutions
@@ -312,13 +311,13 @@ static void TrySolvedConstrained(const Counters& counters,
 
 	solution[nextUnknown] = tentativeSolution;
 
-	TrySolvedConstrained(counters, m, rowToSolve - 1, nextUnknown - 1, constraints, alreadyAssigned, minimumPresses);
+	TrySolvedConstrained(m, rowToSolve - 1, nextUnknown - 1, constraints, alreadyAssigned, minimumPresses);
 }
 
-static void SolveConstrained(const Counters& counters, const vector<vector<int64_t>>& m, const vector<int64_t>& constraints, int64_t *minimumPresses)
+static void SolveConstrained(const vector<vector<int32_t>>& m, const vector<int32_t>& constraints, int32_t* minimumPresses)
 {
-	vector<int64_t> solution(constraints.size(), -1);
-	TrySolvedConstrained(counters, m, min(solution.size() - 1, m.size() - 1), solution.size() - 1, constraints, &solution, minimumPresses);
+	vector<int32_t> solution(constraints.size(), -1);
+	TrySolvedConstrained(m, min(solution.size() - 1, m.size() - 1), solution.size() - 1, constraints, &solution, minimumPresses);
 }
 
 static void Puzzle10_A(const string& filename)
@@ -327,7 +326,7 @@ static void Puzzle10_A(const string& filename)
 	ifstream input(filename);
 	//istringstream input(dummy);
 
-	int64_t answer = 0;
+	int32_t answer = 0;
 
 	string line;
 	while (getline(input, line))
@@ -341,7 +340,7 @@ static void Puzzle10_A(const string& filename)
 		{
 			uint32_t c = combination;
 			uint32_t lights = 0;
-			for (int64_t buttonIndex = 0; buttonIndex < (int64_t)m.Buttons.size(); buttonIndex++)
+			for (int32_t buttonIndex = 0; buttonIndex < (int32_t)m.Buttons.size(); buttonIndex++)
 			{
 				if (c & 1)
 				{
@@ -358,7 +357,7 @@ static void Puzzle10_A(const string& filename)
 		answer += minimumPresses;
 	}
 
-	printf("[2025] Puzzle10_A: %" PRId64 "\n", answer);
+	printf("[2025] Puzzle10_A: %" PRId32 "\n", answer);
 }
 
 static void Puzzle10_B(const string& filename)
@@ -367,49 +366,38 @@ static void Puzzle10_B(const string& filename)
 	ifstream input(filename);
 	//istringstream input(dummy);
 
-	int64_t answer = 0;
+	int32_t answer = 0;
 
 	string line;
 	while (getline(input, line))
 	{
 		Counters counters = ParseCounters(line);
 
-		vector<vector<int64_t>> matrix;
-		while (true)
-		{
-			matrix = CountersToAugmentedMatrix(counters);
-			ReduceAndTrim(&matrix);
+		vector<vector<int32_t>> matrix = CountersToAugmentedMatrix(counters);
+		vector<int32_t> constraints = CountersToConstraints(counters);
 
-			bool allLeadingNonZero = true;
-			for (int i = 0; i < (int)matrix.size(); i++)
+		while (ReduceAndTrim(&matrix) == false)
+		{
+			for (size_t buttonToSwap = 0; buttonToSwap < counters.Buttons.size(); buttonToSwap++)
 			{
-				if (matrix[i][i] == 0)
+				size_t swapWith = rand() % counters.Buttons.size();
+				swap(constraints[buttonToSwap], constraints[swapWith]);
+
+				for (auto& row : matrix)
 				{
-					allLeadingNonZero = false;
-					break;
+					swap(row[buttonToSwap], row[swapWith]);
 				}
 			}
+		}			
 
-			if (allLeadingNonZero)
-				break;
-
-			for (int i = 0; i < (int)counters.Buttons.size(); i++)
-			{
-				swap(counters.Buttons[i], counters.Buttons[rand() % (int)counters.Buttons.size()]);
-			}
-		}
-
-		auto constraints = CountersToConstraints(counters);
-
-		int64_t minimumPresses = numeric_limits<int64_t>::max();
-		SolveConstrained(counters, matrix, constraints, &minimumPresses);
-
-		assert(minimumPresses < numeric_limits<int64_t>::max());
+		int32_t minimumPresses = numeric_limits<int32_t>::max();
+		SolveConstrained(matrix, constraints, &minimumPresses);
+		assert(minimumPresses < numeric_limits<int32_t>::max());
 
 		answer += minimumPresses;
 	}
 
-	printf("[2025] Puzzle10_B: %" PRId64 "\n", answer);
+	printf("[2025] Puzzle10_B: %" PRId32 "\n", answer);
 }
 
 void Puzzle10_2025(const string& filename)
